6a1415817f025c520d3082820d84b3f2a2bf431b
Katy Rosli <katyrosli@gmail.com>
2023-05-03 10:46:33 +0200

    added footer test and pass all footer test

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -19,4 +19,28 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct content', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.footer-page__github').textContent).toContain('Open sourced on Github');
+    expect(compiled.querySelector('.footer-page__website').textContent).toContain('Katy Rosli');
+  });
+
+  it('should open Github link in new tab', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const githubLink = fixture.nativeElement.querySelector('.footer-page__github');
+    expect(githubLink.target).toEqual('_blank');
+    expect(githubLink.getAttribute('rel')).toEqual('noopener noreferrer');
+  })
+
+  it('should open website link in new tab', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const websiteLink = fixture.nativeElement.querySelector('.footer-page__website');
+    expect(websiteLink.target).toEqual('_blank');
+    expect(websiteLink.getAttribute('rel')).toEqual('noopener noreferrer');
+  });
 });
